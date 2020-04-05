9cd453cb8f9b5d5715647f97973a0f2205a5f84f
parvej <parvej_about@outlook.com>
2020-04-05 05:16:08 -0400

    Revert "addTwoNumber program added"
    This reverts commit 4bd124f46b72256175b5920e7ec8fe96d63db83b.

---
+++ Simple-Problem/addTwoNumber.c
@@ -0,0 +1,13 @@
+/**
+ *Simply you have two add 2 numbers. and show the output.
+ */
+
+#include <stdio.h>
+int main(){
+    int num1, num2, sum;
+    num1 = 50;
+    num2 =15;
+    sum = num1 + num2;
+    printf("%d + %d = %d\n", num1, num2, sum);
+    return 0;
+}
